985f81f0fbeb89c252c9a874a9d1d2d71a3f3a66
Harsh <harshchauhan291@gmail.com>
2022-04-08 18:54:20 +0530

    changes

---
+++ redux gamehub/src/components/CardsData.js
@@ -0,0 +1,112 @@
+const Cardsdata = [
+    {
+        id: 1,
+        rname: "GTA 5",
+        imgdata: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvA41DPBGDFSSkZGgeevufwB_J6UVGeIgPW8PjZPPnZOe5aRMPInO0YRHJyk-lHBTT0oA&usqp=CAU",
+        address: "Rockstargames",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 1175 + order placed from here recently",
+        price: 2000,
+        rating: "9.5",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 2,
+        rname: "red dead redemption 2",
+        imgdata: "https://lh3.googleusercontent.com/HCUkD69MAHEOj84Yi7Kb5vxHpCePTsmQI4g9vYuVPUo-87cWE6ZZIk0tiyYzaiS9zaAFMTXRNYJaaRczRN-yQYw",
+        address: "Rockstar Games",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 2525 + order placed from here recently",
+        price: 2500,
+        rating: "8.0",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 3,
+        rname: "Assassin's Creed Unity",
+        imgdata: "https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fassassins-creed-unity%2Fhome%2FACU_UCS14913_EGST_BannerBundle_US_Store_Landscape_2580x1450-2580x1450-24a0303665904010909579042262c383124ad3c1.jpg",
+        address: "Ubisoft Games",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 650 + order placed from here recently",
+        price: 1100,
+        rating: "7.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 4,
+        rname: "Forza Horizon",
+        imgdata: "https://i.pinimg.com/736x/47/0b/a2/470ba2a0278efee802d718e83c89e066.jpg",
+        address: "PlayGround Games",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 300 + order placed from here recently",
+        price: 1000,
+        rating: "8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 5,
+        rname: "God of War",
+        imgdata: "https://image.api.playstation.com/vulcan/img/rnd/202010/2217/RnDzysOs81fdvhdQAHYtxjFT.png",
+        address: "Sony",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "1050 + order placed from here recently",
+        price: 500,
+        rating: "8.5",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 6,
+        rname: "Need For Speed",
+        imgdata: "https://lh3.googleusercontent.com/rSQLAhoVFSiUxztL0k4qGBfuRhcJDAXxCFQWiWLUuIfSBMgIm9JgF6cRgmUZrjVHunziZh-l3nWIca2X0fK-38k",
+        address: "EA Games",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 1100 + order placed from here recently",
+        price: 800,
+        rating: "8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 7,
+        rname: "PlayerUnknown's Battleground",
+        imgdata: "https://m.media-amazon.com/images/I/714vaW2G-ZL._SL1500_.jpg",
+        address: "Tencent Games",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed from here recently",
+        price: 900,
+        rating: "9.0",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 8,
+        rname: "Apex Legends",
+        imgdata: "https://static-cdn.jtvnw.net/ttv-boxart/511224-272x380.jpg",
+        address: "Electronics Art",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed from here recently",
+        price: 900,
+        rating: "8.4",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 9,
+        rname: "Call Of Duty",
+        imgdata: "https://image.api.playstation.com/cdn/UP0002/CUSA04762_00/hvnLMFIUbVVGomXCNLL499iTbDj0RMM9VfWNxnEqJkW4LpFUS1Y0gCLi7LZYxiFD.png",
+        address: "Sony",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "2525 + order placed from here recently",
+        price: 500,
+        rating: "8.5",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+];
+
+export default Cardsdata;
